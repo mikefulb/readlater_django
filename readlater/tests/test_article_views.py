60dde7d3c08c1b25516f5a98dbc3c40ac3adc448
Michael Fulbright <mike.fulbright@pobox.com>
2020-12-24 14:48:33 -0500

    Remove duplicated setting unit test from article tests.

--- readlater/tests/test_article_views.py
+++ readlater/tests/test_article_views.py
@@ -324,35 +324,3 @@ class ArticleDeleteViewTest(TestCase):
 
     def test_article_delete_form_state_unread_valid_post(self):
         self.send_delete_post('read')
-
-
-class SettingsViewTest(TestCase):
-    NUM_CATEGORIES = 5
-
-    @classmethod
-    def setUpTestData(cls):
-        for categ_id in range(SettingsViewTest.NUM_CATEGORIES):
-            Category.objects.create(name=f'Category {categ_id}')
-
-    def test_settings_url_exists(self):
-        response = self.client.get('/readlater/settings/')
-
-        # test that 'Read' is a link to the read page from the unread page
-        self.assertContains(response, '<h4>Settings</h4>', status_code=200)
-
-        # also test using name
-        response = self.client.get(reverse('settings'))
-        self.assertContains(response, '<h4>Settings</h4>', status_code=200)
-
-    def test_settings_uses_correct_template(self):
-        response = self.client.get('/readlater/settings/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'readlater/settings_base.html')
-
-    def test_settings_lists_all_categories(self):
-        response = self.client.get('/readlater/settings/')
-        self.assertEqual(response.status_code, 200)
-
-        # check the number of items in categories list is number created plus one
-        # for the uncategorized category created during migration step
-        self.assertTrue(len(response.context['category_list']) == SettingsViewTest.NUM_CATEGORIES+1)
