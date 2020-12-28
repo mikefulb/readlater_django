cb52a1f8157af73266e77df2fa7674b6525a1566
Michael Fulbright <mike.fulbright@pobox.com>
2020-12-28 13:18:55 -0500

    Further cleanup from previous commit.

--- readlater/tests/functional/test_article.py
+++ readlater/tests/functional/test_article.py
@@ -1,55 +1,14 @@
-import os
 import re
-import time
 import datetime
 from urllib.parse import urljoin
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.test import LiveServerTestCase
 from django.urls import reverse
 
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
-from .utils import wait
+from .utils import wait, FunctionalTestBase
 
 from ...models import Article, Category
-
-# MAX_WAIT = 10
-#
-#
-# def wait(fn):
-#     def modified_fn(*args, **kwargs):
-#         start_time = time.time()
-#         while True:
-#             try:
-#                 return fn(*args, **kwargs)
-#             except (AssertionError, WebDriverException) as e:
-#                 if time.time() - start_time > MAX_WAIT:
-#                     raise e
-#                 time.sleep(0.5)
-#     return modified_fn
-
-
-class FunctionalTestBase:
-
-    def setUp(self):
-        self.selenium = webdriver.Firefox()
-        self.selenium.implicitly_wait(10)
-        self.staging_server = os.environ.get('STAGING_SERVER')
-        if self.staging_server:
-            self.live_server_url = self.staging_server
-
-    def tearDown(self):
-        time.sleep(1)
-        self.selenium.quit()
-        super().tearDown()
-
-    @wait
-    def wait_for(self, fn):
-        fn()
 
 
 class ArticleListTestCase(FunctionalTestBase, StaticLiveServerTestCase):
