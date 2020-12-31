99c8a6522af99c09146c4deee29e28fea318f2b4
Michael Fulbright <mike.fulbright@pobox.com>
2020-12-30 21:42:59 -0500

    Remove debugging statements.

--- readlater_django/settings.py
+++ readlater_django/settings.py
@@ -17,8 +17,6 @@ from decouple import Csv, config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print('BASE_DIR = ', BASE_DIR)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -171,5 +169,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-print('STATICFILES_DIRS = ', STATICFILES_DIRS)
